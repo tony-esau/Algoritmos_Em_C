3cf021d3c0f9a7746d82965add5652d8e202829d
Tony Esaú <tonyesau10@protonmail.com>
2024-02-18 12:15:02 -0300

    ...

---
+++ Estrutura de Dados/Estruturas Dinâmicas/Fila Estática.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define memoria 5
+
+typedef struct No_cabeca{
+    int vetor[memoria];
+    int frente;
+    int retaguarda;
+}Sentinela;
+
+void cria_fila(Sentinela *No_cabeca){
+    No_cabeca->frente = -1;
+    No_cabeca->retaguarda = -1;
+}
+
+int fila_vazia(Sentinela *No_cabeca){
+    if((No_cabeca->frente == -1) && (No_cabeca->retaguarda == -1)){
+        return 1;
+    }else{
+        return 0;
+    } 
+}
+
+int fila_cheia(Sentinela *No_cabeca){
+    if(((No_cabeca->retaguarda + 1) % memoria) == (No_cabeca->frente)){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void inserir_fila(Sentinela *No_cabeca){
+    if(fila_cheia(No_cabeca) == 1){
+        printf("A fila está cheia");
+    }else{
+        if(No_cabeca->frente == -1){
+            No_cabeca->frente+=2;
+        }
+        if(No_cabeca->retaguarda == -1){
+            No_cabeca->retaguarda++;
+        }
+        No_cabeca->retaguarda = (No_cabeca->retaguarda + 1) % memoria;
+        int auxiliar;
+        printf("Digite a chave:");
+        scanf("%d",&auxiliar);
+        fflush(stdin);
+        No_cabeca->vetor[No_cabeca->retaguarda] = auxiliar;
+    }
+}
+
+void remover_fila(Sentinela *No_cabeca){
+    if(fila_vazia(No_cabeca) == 1){
+        printf("A fila ainda não tem elementos");
+    }else{
+        if(No_cabeca->frente == No_cabeca->retaguarda){
+            No_cabeca->frente = -1;
+            No_cabeca->retaguarda = No_cabeca->frente;
+        }else{
+            No_cabeca->frente = (No_cabeca->frente + 1) % memoria;
+        }
+    }
+}
+
+void imprime_fila(Sentinela No_cabeca){
+    Sentinela *ptr_auxiliar = &No_cabeca;
+    if(fila_vazia(ptr_auxiliar) == 0){
+        printf("\nA lista se encontra na seguinte configuração:\n");
+        if(No_cabeca.frente ==  No_cabeca.retaguarda){
+            printf("\n[%d]\n",No_cabeca.vetor[No_cabeca.frente]);
+        }else{
+            printf("[");
+            while(No_cabeca.frente != No_cabeca.retaguarda){
+                printf("%d,",No_cabeca.vetor[No_cabeca.frente]);
+                No_cabeca.frente = ((No_cabeca.frente + 1) % memoria);
+            }
+            printf("%d]\n",No_cabeca.vetor[No_cabeca.retaguarda]);
+        }
+    }else{
+        printf("Ainda não há elementos na fila.");
+    }
+}
+
+void imprime_menu(){
+    printf("\n----------Opções----------\n");
+    printf("1-Inserir...................\n");
+    printf("2-Remover...................\n");
+    printf("3-Imprimir..................\n");
+    printf("4-Limpar Tela...............\n");
+    printf("5-Sair......................\n");
+}
+
+int main(){
+    Sentinela fila;
+    Sentinela *ptr_fila = &fila;
+    cria_fila(ptr_fila);
+    int verificador;
+
+    while(verificador != 5){
+        imprime_menu();
+        scanf("%d",&verificador);
+        fflush(stdin);
+
+        if(verificador == 1){
+            inserir_fila(ptr_fila);
+        }else if(verificador == 2){
+            remover_fila(ptr_fila);
+        }else if(verificador == 3){
+            system("cls");
+            imprime_fila(fila);
+        }else if(verificador == 4){
+            system("cls");
+        }else if(verificador == 5){
+            exit(1);
+        }else{
+           printf("Comando inexistente."); 
+        }
+    }
+   return 0; 
+}
--- Estrutura de Dados/Estruturas Dinâmicas/pilha_dinamica.c
+++ Estrutura de Dados/Estruturas Dinâmicas/pilha_dinamica.c
@@ -1,155 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 //Estrutura de Dados.
 typedef struct Dados_de_No{
-    char nome[50];
     int chave;
 }Dados;
 
-//Estrutura que define um nó.
+//Estrutura que define um nï¿½.
 typedef struct No_de_pilha{
     Dados *dados;
     struct No_de_pilha *ptr_abaixo;
 }No;
 
-//Cria a pilha, inicialmente é um ponteiro para NULL.
+//Cria a pilha, inicialmente um ponteiro para NULL.
 No* cria_pilha(){
     No *auxiliar = NULL;
     return auxiliar;
 }
 
-//Função de ler os dados de um nó.
-No* ler_dados(No *topo){
-    topo = (No*) malloc(sizeof(No));
-    if(topo == NULL){
-        printf("Não foi possivel alocar memória para o nó.");
-        return NULL;
-    }
-    topo->dados = (Dados*) malloc(sizeof(Dados));
-     if(topo->dados == NULL){
-        printf("Não foi possivel alocar memória para os dados do nó.");
-        return NULL;
-    }
-
-    printf("Digite o nome:");
-    fgets(topo->dados->nome,50,stdin);
-    //Retira o '\n' da string;
-    for(int i = 0; (topo->dados->nome[i]) != '\0'; i++){
-        if(topo->dados->nome[i] == '\n'){
-            topo->dados->nome[i] = '\0';
-            break;
-        } 
-    }
-
-    printf("Digite uma chave:");
-    scanf("%d",&(topo->dados->chave));
-
-    return topo;
-}
-
-//Função de Empilhar um nó.
-void push(No **ptr_topo){
-    No* auxiliar = ler_dados(*ptr_topo);
-    if(auxiliar == NULL){
-        exit(1);
+//FunÃ§Ã£o de Empilhar um nÃ³.
+No* empilhar(No *topo, int a){
+    No* novo = (No*)malloc(sizeof(Dados));
+    if(novo != NULL){
+        novo->ptr_abaixo = topo;
+        novo->dados->chave = a;
+        return novo;
     }else{
-        auxiliar->ptr_abaixo = *ptr_topo;
-        *ptr_topo = auxiliar;
+        printf("Falha na alocaÃ§Ã£o de memÃ³ria!");
+        retrn 
     }
 }
 
-//Função de desempilhar um nó.
-void pop(No **ptr_topo){
+//FunÃ§Ã£o de desempilhar um nÃ³.
+No* desempilhar(No **ptr_topo){
+    No* remover = NULL; 
+
     if(*ptr_topo != NULL){
-        No *auxiliar = *ptr_topo;
-        *ptr_topo = auxiliar->ptr_abaixo;
-        free(auxiliar);
+        No *remover = *ptr_topo;
+        *ptr_topo = remover->ptr_abaixo;
     }else{
-        printf("\nAinda não há nós na pilha.\n");
+        printf("\nAinda nao ha nos na pilha.\n");
     }
 }
 
-void imprime_pilha(No *topo){
-    if(topo == NULL){
-        printf("\nAinda não há nós na pilha.\n");
-    }else{
-        int i = 0;
-        while(topo != NULL){
-            if(i == 0 && ((topo->ptr_abaixo) != NULL)){
-                printf("\n[Nome:%s. <-- Topo\n",topo->dados->nome);
-                printf("Chave:%d.]\n",topo->dados->chave);
-                printf("\n   |   \n");
-                printf("   v   \n");
-            }else if(i == 0 && ((topo->ptr_abaixo) == NULL)){
-                printf("\n[Nome:%s. <-- Topo\n",topo->dados->nome);
-                printf("Chave:%d.]\n",topo->dados->chave);
-            }else if((topo->ptr_abaixo) == NULL){
-                printf("\n[Nome:%s.\n",topo->dados->nome);
-                printf("Chave:%d.]\n",topo->dados->chave);
-            }else{
-                printf("\n[Nome:%s.\n",topo->dados->nome);
-                printf("Chave:%d.]\n",topo->dados->chave);
-                printf("\n   |   \n");
-                printf("   v   \n");
-            }
-            i++;
-            topo = topo->ptr_abaixo;
-        }   
-    }
-}
-
-void imprime_menu(){
-    printf("\n----------Opções----------\n");
-    printf("1-Empilhar................\n");
-    printf("2-Desempilhar.............\n");
-    printf("3-Imprimir Pilha..........\n");
-    printf("4-Limpar Tela.............\n");
-    printf("5-Sair do Programa........\n");
-}
 
 int main(){
-    setlocale(LC_ALL,"Portuguese");
 
     No *topo;
     No **ptr_topo = &topo;
-
-    int verificador;
-    printf("Deseja criar uma pilha? (1:Sim|0 ou outro:Não)\n");
-    scanf("%d",&verificador);
-    if(verificador == 1){
-        topo = cria_pilha();
-    }else{
-        printf("Obrigado por utilizar...");
-        exit(1);
-    }
-
-    do{
-        imprime_menu();
-        printf("Digite a opção:\n");
-        scanf("%d",&verificador);
-        fflush(stdin);
-
-        if(verificador == 1){
-            push(ptr_topo);
-            printf("\nNó empilhado com sucesso.\n");
-        }else if(verificador == 2){
-            pop(ptr_topo);
-            printf("\nNó desempilhado com sucesso.\n");
-        }else if(verificador == 3){
-            imprime_pilha(topo);
-        }else if(verificador == 4){
-            system("cls"); //"clear" em ambientes Linux
-        }else if(verificador == 5){
-            system("cls"); 
-            printf("Obrigado por utilizar...");
-            exit(1);
-        }else{
-            printf("Opção inválida\n");
-        }
-
-    }while(verificador != 5);
 
     return 0;
 }
