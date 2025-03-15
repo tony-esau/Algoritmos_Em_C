69fa9ab575689612ef4ceb8b79ac1233ced3cb96
tony-esau <143760130+tony-esau@users.noreply.github.com>
2025-03-15 09:51:03 -0300

    Update fatorial.c

--- Estruturas_E_Algoritmos/Recursão/fatorial.c
+++ Estruturas_E_Algoritmos/Recursão/fatorial.c
@@ -1,5 +1,5 @@
 //Autor: Tony Esaú de Oliveira© CC
-//26/02/2024
+//26/02/2025
 /*
 Implementa um algoritmo simples de cálculo de fatorial com 
 base no livro Estruturas de Dados e Seus Algoritmos de Szwarcfiter.
